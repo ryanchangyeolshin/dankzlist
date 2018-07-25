2da990cbf61160c9d56bd26136ac547cc8c757a1
changyeolshin <changyeolshin@gmail.com>
2018-07-24 18:35:02 -0700

    Created the routes to create and update items.

--- server/routers/items-router.js
+++ server/routers/items-router.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: './.env' });
 const { Router } = require('express');
+const validateToken = require('../middleware/validate-token');
 const {
   getFirstHundredItems,
   getItemsByUserID,
@@ -20,6 +21,43 @@ const ItemsRouter = () => {
       catch(err) {
         res.status(500).json({ message: err });
       }
+    })
+    .get('/:user_id', async (req, res) => {
+      const { user_id } = req.params;
+      try {
+        const items = await getItemsByUserID(user_id);
+        res.status(200).json(items);
+      }
+      catch (err) {
+        res.status(500).json({ message: err });
+      }
+    })
+    .post('/:user_id', validateToken, async (req, res) => {
+      const { user_id } = req.params;
+      const { itemData } = req.body;
+      const payload = { user_id, ...itemData };
+      try {
+        const response = await createItem(payload);
+        res.status(201).json({ response });
+      }
+      catch(err) {
+        res.status(500).json({ message: err });
+      }
+    })
+    .put('/:user_id/:item_id', validateToken, async (req, res) => {
+      const { item_id } = req.params;
+      const { payload } = req.body;
+      try {
+        const updated = await updateItem(item_id, payload);
+        res.status(202).json({ updated });
+      }
+      catch(err) {
+        res.status(500).json({ message: err });
+      }
+    })
+    .delete('/:user_id/:item_id', validateToken, async (req, res) => {
+      const { item_id } = req.params;
+
     });
 
   return router;
