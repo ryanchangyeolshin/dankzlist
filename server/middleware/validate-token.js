06f05b6816f613e3a5bd97b18e1f373ad8339f81
changyeolshin <changyeolshin@gmail.com>
2018-07-24 18:34:16 -0700

    Created the validateToken middleware function.

--- server/middleware/validate-token.js
+++ server/middleware/validate-token.js
@@ -1,2 +1,18 @@
+require('dotenv').config({ path: './.env' });
+const jwt = require('jsonwebtoken');
 
+const validateToken = (req, res, next) => {
+  if (req.body.token) {
+    const { token } = req.body;
+    return jwt.verify(token, process.env.JWT_TOKEN_KEY, (err, decoded) => {
+      if (err) {
+        res.status(500).json({ message: err });
+      }
+      next();
+    });
+  }
+  res.status(404).json({ message: "Token is not available! User must login!" });
+};
+
+module.exports = validateToken;
 
